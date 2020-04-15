89c7ce8af2f88ad2b1301b8a9a293fe9dbe80263
robindave1 <61278408+robindave1@users.noreply.github.com>
2020-04-15 15:29:13 +0200

    mapbox
    functies toegevoegd

--- script.js
+++ script.js
@@ -4,18 +4,37 @@ mapboxgl.accessToken = 'pk.eyJ1Ijoicm9iaW4xNyIsImEiOiJjazkxOGFveGkwOGtzM2ZwN2ZyO
 // Initialate map
 var map = new mapboxgl.Map({
   container: 'map',
-  style: 'mapbox://styles/mapbox/streets-v11',
-  //style: 'mapbox://styles/mapbox/satellite-v9'
-  // style: 'mapbox://styles/mapbox/dark-v10'
-  // pitch: 45,
-  // bearing: -17.6,
-  // Positioning the map on a certain longitute + latitude and zooming in
-  // Let op de volgorde van de lat, lon!!
+  style: 'mapbox://styles/robin17/ck91a3syy106u1ioaa6baw45a',
   center: [4.322840, 52.067101],
   zoom: 15,
 
 });
 
+  var popup = new mapboxgl.Popup()
+     .setHTML('<h3>De Haagse Hogeschool</h3><p>Is momenteel dicht.</p>');
+
+ 
+ var marker = new mapboxgl.Marker()
+   .setLngLat([4.324439, 52.067200])
+   .setPopup(popup)
+  .addTo(map);
+
+
+// Voeg de planner toe
+map.addControl(
+  new MapboxDirections({
+    accessToken: mapboxgl.accessToken
+  }),
+  'bottom-left'
+);
+
+// Voeg de zoekbalk toe
+map.addControl(
+  new MapboxGeocoder({
+    accessToken: mapboxgl.accessToken,
+    mapboxgl: mapboxgl
+  })
+);
 
 // Add zoom and rotation controls to the map.
 map.addControl(new mapboxgl.NavigationControl());
